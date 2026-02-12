74d49b0bc3e357c27ace47f6407ec0ff1985b87c
Sayed Ashfaq <302syedashfaq@gmail.com>
2026-02-12 23:12:36 +0530

    Updated the app with models

--- router.py
+++ router.py
@@ -1,14 +1,23 @@
 from langchain.chat_models import init_chat_model
 from config.settings import settings
+from dotenv import load_dotenv
+
+
 
 _models = {}
 
 def get_llm(role: str):
+    load_dotenv()
     if role not in _models:
         cfg = settings.data["llm"][role]
         _models[role] = init_chat_model(
             model=cfg["model_name"],
             model_provider=cfg["provider"],
-            temperature=cfg["temperature"]
+            temperature=cfg["temperature"], 
+
         )
     return _models[role]
+
+
+if __name__ == "__main__":
+    print(get_llm("reasoning").invoke("Hey there, testing!!"))
